0de86bde8f949b35fdc61e533cc240fa541e27fe
Noah Saso <6721426+NoahSaso@users.noreply.github.com>
2025-05-08 17:43:37 -0400

    added ^ to solidity version

--- src/UrdFactory.sol
+++ src/UrdFactory.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity 0.8.19;
+pragma solidity ^0.8.19;
 
 import {IUrdFactory} from "./interfaces/IUrdFactory.sol";
 import {IUniversalRewardsDistributor} from "./interfaces/IUniversalRewardsDistributor.sol";
